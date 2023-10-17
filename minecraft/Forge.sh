9fdf635580d1f506566db7f204b2f401fe8b3911
Mindless-Gaming <144237536+Mindless-Gaming@users.noreply.github.com>
2023-10-17 11:38:06 +0200

    Update Forge.sh

--- minecraft/Forge.sh
+++ minecraft/Forge.sh
@@ -6,8 +6,9 @@ sleep 5
 
 echo "mache ein Update der Ubuntu Respositories."
 sleep 2
-
 sudo apt update
 sudo apt upgrade -y
 
+echo "Downloade Forge Server Daten"
+sleep 1
 wget https://download1532.mediafire.com/dmm0ira2hyxgoyVNtG_j09QS2XmJL28OPFe9XQrIf7M0KQaXX52kxLbXdVM8MPeTdtx0USRSru72KdKdbdDmRckCrspYVcuxX7_N1yHKdCB7SzF-KibzVw-Z4EzB5TfMucLaMGSUVosPrLbSPdSpKLNaiOPjr7WacZ4pVBzofRHlmdY/ov9ezhq8pim8bik/ForgesServer.tar.gz
