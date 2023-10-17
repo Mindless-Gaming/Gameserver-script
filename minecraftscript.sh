80fd3d994aea644b0859950244b31d7893b67090
Mindless-Gaming <144237536+Mindless-Gaming@users.noreply.github.com>
2023-10-17 11:04:41 +0200

    Update minecraftscript.sh

--- minecraftscript.sh
+++ minecraftscript.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "###################################################################"
-echo "#Script Name	: MCServerInstaller                "                                                                          
-echo "#Description	: A powerful bash script for easy installation of a Minecraft server (Vanilla, Forge, Spigot & Paper)   "                                                                                                                                                                
+echo "#Script Name  	: MCServerInstaller                "                                                                          
+echo "#Description   	: A powerful bash script for easy installation of a Minecraft server (Vanilla, Forge, Spigot & Paper)   "                                                                                                                                                                
 echo "#Author       	: MindlessGamingDE       "                                       
-echo "#GitHub         : https://github.com/Mindless-Gaming "                                        
+echo "#GitHub           : https://github.com/Mindless-Gaming "                                        
 echo "###################################################################"
 echo
 echo "1. Minecraft Forge"
