bf5ff4c6c5cdd5b0b6167aefdd43f9a2caa05dd5
Mindless-Gaming <144237536+Mindless-Gaming@users.noreply.github.com>
2023-10-17 13:23:38 +0200

    Update Gameserver-installer.sh

--- Gameserver-installer.sh
+++ Gameserver-installer.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+echo "###################################################################"
+echo "#Script Name  	: MCServerInstaller                "                                                                          
+echo "#Description   	: A powerful bash script for easy installation of a Minecraft server (Vanilla, Forge, Spigot & Paper)   "                                                                                                                                                                
+echo "#Author       	: MindlessGamingDE       "                                       
+echo "#GitHub           : https://github.com/Mindless-Gaming "                                        
+echo "###################################################################"
+echo
