2e828c3a9b7192ca0179bf86344d19115b588370
Mindless-Gaming <144237536+Mindless-Gaming@users.noreply.github.com>
2023-10-17 11:51:44 +0200

    Update Forge.sh

--- minecraft/Forge.sh
+++ minecraft/Forge.sh
@@ -13,6 +13,9 @@ echo "Downloade Forge Server Daten"
 sleep 1
 wget https://download1532.mediafire.com/dmm0ira2hyxgoyVNtG_j09QS2XmJL28OPFe9XQrIf7M0KQaXX52kxLbXdVM8MPeTdtx0USRSru72KdKdbdDmRckCrspYVcuxX7_N1yHKdCB7SzF-KibzVw-Z4EzB5TfMucLaMGSUVosPrLbSPdSpKLNaiOPjr7WacZ4pVBzofRHlmdY/ov9ezhq8pim8bik/ForgesServer.tar.gz
 
+mkdir forgeserver
+cd forgeserver
+
 echo "entpacke die Forge Server Daten"
 sleep 1
 tar -xf ForgesServer.tar.gz
