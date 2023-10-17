a4553b1a932653ac90190f59e56e02d96c25facb
Mindless-Gaming <144237536+Mindless-Gaming@users.noreply.github.com>
2023-10-17 13:28:18 +0200

    Update Gameserver-installer.sh

--- Gameserver-installer.sh
+++ Gameserver-installer.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+echo "###################################################################"
+echo "#Script Name  	: Gameserver-installer"                                                                                          
+echo "#Description	  : A powerful bash script for easy installation of some Gameservers"                                                                                                                                                                   
+echo "#Author       	: MindlessGamingDE"                                              
+echo "#GitHub         : https://github.com/Mindless-Gaming"     
+echo "#Discord        : (soon)"
+echo "###################################################################"
