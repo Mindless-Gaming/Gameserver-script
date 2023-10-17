261e976acf5c6cf95d3aeeca7f015f1352cf6ddc
Mindless-Gaming <144237536+Mindless-Gaming@users.noreply.github.com>
2023-10-17 12:49:29 +0200

    Update auswahlscript-script.sh

--- minecraft/auswahlscript-script.sh
+++ minecraft/auswahlscript-script.sh
@@ -18,15 +18,15 @@ if [ $choice == "1" ]
 fi
 if [ $choice == "2" ]
     then
-    bash <(curl -s https://raw.githubusercontent.com/Sigma-Production/PteroFreeStuffinstaller/V1.10.1/resources/Addons.sh)
+    #bash <(curl -s https://raw.githubusercontent.com/Sigma-Production/PteroFreeStuffinstaller/V1.10.1/resources/Addons.sh)
 fi
 if [ $choice == "3" ]
     then
-    echo "Installing Particles themes..."
-    bash <(curl -sSL https://raw.githubusercontent.com/Sigma-Production/PteroFreeStuffinstaller/V1.10.1/resources/ParticleJS/install.sh)
+    #echo "Installing Particles themes..."
+    #bash <(curl -sSL https://raw.githubusercontent.com/Sigma-Production/PteroFreeStuffinstaller/V1.10.1/resources/ParticleJS/install.sh)
 fi
 if [ $choice == "4" ]
     then
-    echo "Installing Minecraft Theme..."
-    bash <(curl -sSL https://raw.githubusercontent.com/Sigma-Production/PteroFreeStuffinstaller/V1.10.1/resources/mcTheme/install.sh)
+    #echo "Installing Minecraft Theme..."
+    #bash <(curl -sSL https://raw.githubusercontent.com/Sigma-Production/PteroFreeStuffinstaller/V1.10.1/resources/mcTheme/install.sh)
 fi
