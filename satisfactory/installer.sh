3ff23398e10f38f77d823e5224a70204d9f3aad0
Mindless-Gaming <144237536+Mindless-Gaming@users.noreply.github.com>
2023-10-17 14:05:31 +0200

    Update installer.sh

--- satisfactory/installer.sh
+++ satisfactory/installer.sh
@@ -2,10 +2,10 @@
 
 echo "########################-Satisfactory-########################"
 
-dpkg --add-architecture i386
-apt update
-apt-get install steamcmd -y
-ln -s /usr/games/steamcmd steamcmd
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt-get install steamcmd -y
+sudo ln -s /usr/games/steamcmd steamcmd
 
 ./steamcmd +force_install_dir /usr/games/satisfactory +login anonymous +app_update 1690800 validate +quit
 
