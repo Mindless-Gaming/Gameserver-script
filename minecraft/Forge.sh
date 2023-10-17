3b77147e611081b3284afea5df9354f13f88829f
Mindless-Gaming <144237536+Mindless-Gaming@users.noreply.github.com>
2023-10-17 11:31:34 +0200

    Update Forge.sh

--- minecraft/Forge.sh
+++ minecraft/Forge.sh
@@ -5,5 +5,6 @@ echo "This script will install Java17!"
 sleep 5
 
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
 
+wget https://download1532.mediafire.com/dmm0ira2hyxgoyVNtG_j09QS2XmJL28OPFe9XQrIf7M0KQaXX52kxLbXdVM8MPeTdtx0USRSru72KdKdbdDmRckCrspYVcuxX7_N1yHKdCB7SzF-KibzVw-Z4EzB5TfMucLaMGSUVosPrLbSPdSpKLNaiOPjr7WacZ4pVBzofRHlmdY/ov9ezhq8pim8bik/ForgesServer.tar.gz
