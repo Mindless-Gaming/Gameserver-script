ec966b0b35f947dc9c215b320a07bd79e57e4800
Mindless-Gaming <144237536+Mindless-Gaming@users.noreply.github.com>
2023-10-20 12:10:05 +0200

    Update installer.sh

--- code-server/installer.sh
+++ code-server/installer.sh
@@ -1,7 +1,11 @@
 
 echo "#######################-Code-Server-#########################"
 
-echo "Dieses Script benötigt Docker!! Wenn du Docker noch nicht installiert hast, brich den Vorgang ab und installiere es!"
-echo "Dieses Script wird in 30 Sekunden weiter ausgeführt!" 
+echo "lade docker und docker-compose runter" 
+apt install docker docker-compose -y
+
+echo "lade die docker-compose runter"
+wget https://raw.githubusercontent.com/Mindless-Gaming/Gameserver-script/main/code-server/docker-compose.yml
 
 echo "starte den code-server"
+docker-compose up -d
