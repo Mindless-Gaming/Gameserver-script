87f231739a682714f7ba42a7c79e905bc9f8a6c2
Mindless-Gaming <144237536+Mindless-Gaming@users.noreply.github.com>
2023-10-20 12:00:27 +0200

    Update installer.sh

--- code-server/installer.sh
+++ code-server/installer.sh
@@ -1,4 +1,5 @@
 
 echo "#######################-Code-Server-#########################"
 
-echo "Für dieses script wird docker benötigt. Wenn du, dass noch nicht 
+echo "\033[Dieses Script benötigt Docker!! Wenn du Docker noch nicht installiert hast, brich den Vorgang ab und installiere es!\033[0m"
+echo "Dieses Script wird in 30 Sekunden weiter ausgeführt!" 
