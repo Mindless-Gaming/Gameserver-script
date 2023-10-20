51b201f2c7c93f6eb5bb214c8e37db83be977a66
Mindless-Gaming <144237536+Mindless-Gaming@users.noreply.github.com>
2023-10-20 12:01:34 +0200

    Update Gameserver-installer.sh

--- Gameserver-installer.sh
+++ Gameserver-installer.sh
@@ -10,12 +10,17 @@ echo "###################################################################"
 
 echo "1. Minecraft Server erstellen"
 echo "2. Satisfactory Server erstellen"
+echo "3. Coder-Server erstellen (benötigt docker)"
 read -p "Please enter a number: " choice
 if [ $choice == "1" ]
     then
     bash <(curl -s https://raw.githubusercontent.com/Mindless-Gaming/Gameserver-script/main/minecraft/mc-auswahlscript-script.sh)
 fi
 if [ $choice == "2" ]
+    then
+    bash <(curl -s https://raw.githubusercontent.com/Mindless-Gaming/Gameserver-script/main/satisfactory/installer.sh)
+fi
+if [ $choice == "3" ]
     then
     bash <(curl -s https://raw.githubusercontent.com/Mindless-Gaming/Gameserver-script/main/satisfactory/installer.sh)
 fi
