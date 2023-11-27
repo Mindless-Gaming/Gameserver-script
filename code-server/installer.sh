ddc357c6140c7d88337b50be05126fbca6545c7d
Mindless-Gaming <144237536+Mindless-Gaming@users.noreply.github.com>
2023-11-27 21:24:05 +0100

    installer.sh aktualisieren

--- code-server/installer.sh
+++ code-server/installer.sh
@@ -10,4 +10,5 @@ wget https://raw.githubusercontent.com/Mindless-Gaming/Gameserver-script/main/co
 echo "starte den code-server"
 docker-compose up -d
 
-echo "Der Code-Server ist unter https://<ip-adresse>:8449
+echo "Der Code-Server ist unter https://<ip-adresse>:1200 erreichbar"
+wait 5
